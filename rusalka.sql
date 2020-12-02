351eea0c20ea49c913f0862f5447de907f07fc34
Adyputra <adynugrahaputra@gmail.com>
2020-12-02 23:23:16 +0700

    Update rusalka.sql

--- rusalka.sql
+++ rusalka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Des 2020 pada 11.59
+-- Waktu pembuatan: 02 Des 2020 pada 17.22
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.2
 
@@ -43,11 +43,11 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Kemeja Biru', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 10, 'Gambar_1.jpg'),
+(1, 'Kemeja Biru', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 8, 'Gambar_1.jpg'),
 (2, 'Kemeja Biru Tua', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 10, 'Gambar_2.jpg'),
 (3, 'Kemeja Hitam', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 10, 'Gambar_3.jpg'),
 (4, 'Baju Motif', 'Baju Lengan Panjang', 'Pakaian Wanita', 40000, 10, 'Gambar_4.jpg'),
-(5, 'Baju Pink', 'Baju Lengan Panjang', 'Pakaian Wanita', 45000, 10, 'Gambar_5.jpg'),
+(5, 'Baju Pink', 'Baju Lengan Panjang', 'Pakaian Wanita', 45000, 9, 'Gambar_5.jpg'),
 (6, 'Outer Biru', 'Baju Lengan Panjang', 'Pakaian Wanita', 50000, 10, 'Gambar_6.jpg'),
 (7, 'Sandal anak', 'Sandal Anak Pria', 'Pakaian Anak-Anak', 20000, 30, 'Gambar_7.jpg');
 
@@ -75,7 +75,9 @@ INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VA
 (3, 'Meilinnia Fortuna Astri', 'Jember, Jawa Timur', '2020-12-01 19:51:39', '1970-01-01 07:00:00'),
 (4, 'uyyy', 'jemb', '2020-12-01 19:56:29', '1970-01-01 07:00:00'),
 (5, 'test', 'Jember, Jawa Timur', '2020-12-01 20:15:25', '2020-12-02 20:15:25'),
-(6, 'test 2', 'Situbondo, Jawa Timur', '2020-12-01 20:26:42', '2020-12-02 20:26:42');
+(6, 'test 2', 'Situbondo, Jawa Timur', '2020-12-01 20:26:42', '2020-12-02 20:26:42'),
+(7, '', '', '2020-12-02 23:17:49', '2020-12-03 23:17:49'),
+(8, '', '', '2020-12-02 23:18:34', '2020-12-03 23:18:34');
 
 -- --------------------------------------------------------
 
@@ -106,7 +108,20 @@ INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `h
 (6, 3, 2, 'Kemeja Biru Tua', 1, 35000, ''),
 (7, 4, 2, 'Kemeja Biru Tua', 1, 35000, ''),
 (8, 5, 1, 'Kemeja Biru', 1, 35000, ''),
-(9, 6, 4, 'Baju Motif', 1, 40000, '');
+(9, 6, 4, 'Baju Motif', 1, 40000, ''),
+(10, 7, 1, 'Kemeja Biru', 2, 35000, ''),
+(11, 8, 5, 'Baju Pink', 1, 45000, '');
+
+--
+-- Trigger `tb_pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `tb_pesanan` FOR EACH ROW BEGIN
+	UPDATE tb_barang SET stok = stok-NEW.jumlah
+    WHERE id_brg = NEW.id_brg;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -144,13 +159,13 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT untuk tabel `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
