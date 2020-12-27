35915a7bedf91ea73fec361bdcc6cea8fc8c6312
yais01 <55521873+yais01@users.noreply.github.com>
2020-12-27 18:19:21 +0700

    db

--- rusalka.sql
+++ rusalka.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Des 2020 pada 15.56
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.2
+-- Waktu pembuatan: 27 Des 2020 pada 12.18
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,6 +34,7 @@ CREATE TABLE `tb_barang` (
   `kategori` varchar(60) NOT NULL,
   `harga` int(11) NOT NULL,
   `stok` int(4) NOT NULL,
+  `rating` int(11) NOT NULL,
   `gambar` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,14 +42,14 @@ CREATE TABLE `tb_barang` (
 -- Dumping data untuk tabel `tb_barang`
 --
 
-INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Kemeja Biru', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 8, 'Gambar_1.jpg'),
-(2, 'Kemeja Biru Tua', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 10, 'Gambar_2.jpg'),
-(3, 'Kemeja Hitam', 'Kemeja Lengan Pendek', 'Pakaian Pria', 35000, 10, 'Gambar_3.jpg'),
-(4, 'Baju Motif', 'Baju Lengan Panjang', 'Pakaian Wanita', 40000, 10, 'Gambar_4.jpg'),
-(5, 'Baju Pink', 'Baju Lengan Panjang', 'Pakaian Wanita', 45000, 9, 'Gambar_5.jpg'),
-(6, 'Outer Biru', 'Baju Lengan Panjang', 'Pakaian Wanita', 50000, 10, 'Gambar_6.jpg'),
-(7, 'Sandal anak', 'Sandal Anak Pria', 'Pakaian Anak-Anak', 20000, 30, 'Gambar_7.jpg');
+INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `rating`, `gambar`) VALUES
+(1, 'Kemeja Biru', 'Kemeja Lengan Pendek', '1', 35000, 8, 0, 'Gambar_1.jpg'),
+(2, 'Kemeja Biru Tua', 'Kemeja Lengan Pendek', '1', 35000, 10, 0, 'Gambar_2.jpg'),
+(3, 'Kemeja Hitam', 'Kemeja Lengan Pendek', '1', 35000, 10, 0, 'Gambar_3.jpg'),
+(4, 'Baju Motif', 'Baju Lengan Panjang', '2', 40000, 9, 0, 'Gambar_4.jpg'),
+(5, 'Baju Pink', 'Baju Lengan Panjang', '2', 45000, 9, 2, 'Gambar_5.jpg'),
+(6, 'Outer Biru', 'Baju Lengan Panjang', '2', 50000, 10, 3, 'Gambar_6.jpg'),
+(7, 'Sandal anak', 'Sandal Anak Pria', '3', 20000, 30, 5, 'Gambar_7.jpg');
 
 -- --------------------------------------------------------
 
@@ -60,24 +60,35 @@ INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`
 CREATE TABLE `tb_invoice` (
   `id` int(11) NOT NULL,
   `nama` varchar(56) NOT NULL,
-  `alamat` varchar(225) NOT NULL,
+  `provinsi` varchar(255) NOT NULL,
+  `kabupaten` varchar(255) NOT NULL,
+  `jalan` varchar(255) NOT NULL,
+  `nomer` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `note` varchar(255) NOT NULL,
   `tgl_pesan` datetime NOT NULL,
   `batas_bayar` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `tb_invoice`
+-- Struktur dari tabel `tb_kategori`
 --
 
-INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
-(1, 'Ady Nugraha Putra', 'Situbondo, Jawa Timur', '2020-12-01 19:26:43', '1970-01-01 07:00:00'),
-(2, 'Ady Nugraha Putra', 'Situbondo, Jawa Timur', '2020-12-01 19:47:43', '1970-01-01 07:00:00'),
-(3, 'Meilinnia Fortuna Astri', 'Jember, Jawa Timur', '2020-12-01 19:51:39', '1970-01-01 07:00:00'),
-(4, 'uyyy', 'jemb', '2020-12-01 19:56:29', '1970-01-01 07:00:00'),
-(5, 'test', 'Jember, Jawa Timur', '2020-12-01 20:15:25', '2020-12-02 20:15:25'),
-(6, 'test 2', 'Situbondo, Jawa Timur', '2020-12-01 20:26:42', '2020-12-02 20:26:42'),
-(7, '', '', '2020-12-02 23:17:49', '2020-12-03 23:17:49'),
-(8, '', '', '2020-12-02 23:18:34', '2020-12-03 23:18:34');
+CREATE TABLE `tb_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `kategori`) VALUES
+(1, 'Pakaian Pria'),
+(2, 'Pakaian Wanita'),
+(3, 'Pakaian Anak-anak\r\n');
 
 -- --------------------------------------------------------
 
@@ -110,7 +121,8 @@ INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `h
 (8, 5, 1, 'Kemeja Biru', 1, 35000, ''),
 (9, 6, 4, 'Baju Motif', 1, 40000, ''),
 (10, 7, 1, 'Kemeja Biru', 2, 35000, ''),
-(11, 8, 5, 'Baju Pink', 1, 45000, '');
+(11, 8, 5, 'Baju Pink', 1, 45000, ''),
+(12, 9, 4, 'Baju Motif', 1, 40000, '');
 
 --
 -- Trigger `tb_pesanan`
@@ -133,7 +145,7 @@ CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `role_id` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -144,7 +156,8 @@ CREATE TABLE `tb_user` (
 INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
 (1, 'admin', 'admin', '123', 1),
 (2, 'user', 'user', '123', 2),
-(3, 'Ady', 'Ady04', '123', 2);
+(3, 'Ady', 'Ady04', '123', 2),
+(4, 'sososo', 'asd', '$2y$10$U.yecSUMMnCyziizy1mwkO4DiQeNZHCuFRaaEGGIK0f4oJn1oJyEq', 2);
 
 --
 -- Indexes for dumped tables
@@ -161,6 +174,12 @@ ALTER TABLE `tb_barang`
 --
 ALTER TABLE `tb_invoice`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
 
 --
 -- Indeks untuk tabel `tb_pesanan`
@@ -188,19 +207,25 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT untuk tabel `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
